87a9f6e8426c56b296552d7128bb0c5e3a755a92
Shangjin Tang <shangjin.tang@gmail.com>
2022-10-29 22:26:34 +0800

    cmake_official_tutorial: add solution for S2E1 - Creating a Library

--- build_tools/cmake_official_tutorial/Step2/tutorial.cxx
+++ build_tools/cmake_official_tutorial/Step2/tutorial.cxx
@@ -8,6 +8,7 @@
 // TODO 11: Only include MathFunctions if MY_MATH is defined
 
 // TODO 5: Include MathFunctions.h
+#include "MathFunctions.h"
 
 int main(int argc, char* argv[])
 {
@@ -27,7 +28,9 @@ int main(int argc, char* argv[])
   // TODO 6: Replace sqrt with mysqrt
 
   // calculate square root
-  const double outputValue = sqrt(inputValue);
+  /* const double outputValue = sqrt(inputValue); */
+  const double outputValue = mysqrt(inputValue);
+  
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
