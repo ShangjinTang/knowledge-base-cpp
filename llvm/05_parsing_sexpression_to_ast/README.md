d1ac124440a00f0795af8fd6742947fefa40c10c
Shangjin Tang <shangjin.tang@gmail.com>
2023-09-15 08:53:01 +0800

    add llvm/05_parsing_sexpression_to_ast

---
+++ llvm/05_parsing_sexpression_to_ast/README.md
@@ -0,0 +1,9 @@
+# README
+
+`syntax-cli` is required for this chapter.
+
+Installation:
+
+```bash
+npm install --globnal syntax-cli
+```
---
+++ llvm/05_parsing_sexpression_to_ast/eva-llvm.cpp
@@ -0,0 +1,25 @@
+/**
+ * Eva LLVM executable.
+ */
+#include "./src/EvaLLVM.h"
+
+int main(int argc, char *argv[]) {
+    /**
+     * Program to execute.
+     */
+    std::string program = R"(
+        (printf "Value: %d\n" 42)
+    )";
+
+    /**
+     * Compiler instance.
+     */
+    EvaLLVM vm;
+
+    /**
+     * Generate LLVM IR.
+     */
+    vm.exec(program);
+
+    return 0;
+}
