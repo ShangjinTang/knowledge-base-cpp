cd4097f3ee62416f8adf97f490bb749ad0454a0e
Shangjin Tang <shangjin.tang@gmail.com>
2025-01-08 14:48:40 +0800

    chore: fix typo

--- tips_and_tricks/timing_in_block/main.cpp
+++ tips_and_tricks/timing_in_block/main.cpp
@@ -6,13 +6,15 @@ struct Timer {
     std::chrono::system_clock::time_point start, end;
     std::chrono::duration<float> duration;
 
-    Timer() { start = std::chrono::high_resolution_clock::now(); }
+    Timer() {
+        start = std::chrono::high_resolution_clock::now();
+    }
 
     ~Timer() {
         end = std::chrono::high_resolution_clock::now();
         duration = end - start;
         float ms = duration.count() * 1000.0f;
-        std::cout << "Timer took " << ms << "s" << std::endl;
+        std::cout << "Timer took " << ms << "ms" << std::endl;
     }
 };
 
